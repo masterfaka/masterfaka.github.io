e673fb1bfd31ea51bd15ae82bed54ff87d3595a9
REdward <REdwardWeb@gmail.com>
2020-08-05 20:15:42 +0200

    fix1

--- AngularJs/php/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'aashu-main.class.php';
-$_POST = $_POST = json_decode(file_get_contents('php://input'), true);
-$aashuMain = new AashuMain('../data/page.json', $_POST);
-print $aashuMain->$_GET['action']();
