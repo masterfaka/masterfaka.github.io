7fd62e47b4f311e741a8d9f42d6d10f29dce4607
REdward <REdwardWeb@gmail.com>
2020-08-06 21:33:22 +0200

    Fix: headers angularjs. mail cant work on github for php or ajax post

--- angularjs/php/index.php
+++ angularjs/php/index.php
@@ -3,3 +3,4 @@ require_once 'aashu-main.class.php';
 $_POST = $_POST = json_decode(file_get_contents('php://input'), true);
 $aashuMain = new AashuMain('../data/page.json', $_POST);
 print $aashuMain->$_GET['action']();
+
